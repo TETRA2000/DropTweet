a46d91a870284e1774731034bc4eb73f91a5bed0
TETRA2000 <sagittariusm25@gmail.com>
2013-10-30 23:22:17 +0900

    MainActivityをScalaで書き換え

---
+++ DropTweet/src/com/example/droptweet/MainActivity.scala
@@ -0,0 +1,13 @@
+package com.example.droptweet
+
+import android.os.Bundle;
+import android.app.Activity
+
+class MainActivity extends Activity {
+  
+  override def onCreate(savedInstanceState:Bundle) = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+  
+}
