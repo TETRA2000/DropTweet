0abd11120b7cdfcf6e343be451403c3879be79ae
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 11:27:33 +0900

    AuthActivityを実装(以前作ったコピー)

--- DropTweet/src/com/example/droptweet/ui/auth/AuthActivity.java
+++ DropTweet/src/com/example/droptweet/ui/auth/AuthActivity.java
@@ -1,10 +1,51 @@
 package com.example.droptweet.ui.auth;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-public class AuthActivity extends Activity {
+import com.example.droptweet.R;
+
+import twitter4j.TwitterException;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationContext;
+
+public class AuthActivity extends Activity implements Runnable {
+    private static final String CONSUMER_KEY = "";
+    private static final String CONSUMER_SECRET = "";
+
+    public static RequestToken _req;
+    public static OAuthAuthorization _oauth;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        findViewById(R.id.button_auth).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(AuthActivity.this).start();
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        Configuration conf = ConfigurationContext.getInstance();
+
+        _oauth = new OAuthAuthorization(conf);
+        _oauth.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+        try {
+            _req = _oauth.getOAuthRequestToken("callback://CallBackActivity");
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
+        String _uri;
+        _uri = _req.getAuthorizationURL();
+        startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(_uri)), 0);
     }
 }
