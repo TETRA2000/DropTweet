67bca377652e0cbc24de7521ff29a4b1d5b0c03f
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 19:18:02 +0900

    AccountManagerを実装(未テスト)

--- DropTweet/src/com/example/droptweet/ui/twitter/AccountManager.java
+++ DropTweet/src/com/example/droptweet/ui/twitter/AccountManager.java
@@ -1,26 +1,41 @@
 package com.example.droptweet.ui.twitter;
 
-import android.app.ActivityManager;
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.droptweet.Const;
 
 public class AccountManager {
-    public AccountManager(Context context) {
+    private SharedPreferences mPref;
 
+    public AccountManager(Context context) {
+        mPref = context.getSharedPreferences(Const.PREF_NAME, Context.MODE_PRIVATE);
     }
 
     public boolean hasAccount() {
-
+        return mPref.contains(Const.KEY_USER_NAME);
     }
 
     public void setAccount(Account account) {
-
+        SharedPreferences.Editor editor = mPref.edit();
+        editor.putString(Const.KEY_USER_NAME, account.name);
+        editor.putString(Const.KEY_TOKEN, account.token);
+        editor.putString(Const.KEY_TOKEN_SECRET, account.secret);
+        editor.commit();
     }
 
     public Account getAccount() {
-
+        String name = mPref.getString(Const.KEY_USER_NAME, null);
+        String token = mPref.getString(Const.KEY_TOKEN, null);
+        String tokenSecret = mPref.getString(Const.KEY_TOKEN_SECRET, null);
+        return new Account(name, token, tokenSecret);
     }
 
     public void removeAccount() {
-
+        SharedPreferences.Editor editor = mPref.edit();
+        editor.remove(Const.KEY_USER_NAME);
+        editor.remove(Const.KEY_TOKEN);
+        editor.remove(Const.KEY_TOKEN_SECRET);
+        editor.commit();
     }
 }
