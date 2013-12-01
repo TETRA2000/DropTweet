9d021eb77d2c7d17fe6ef764f735fcd3c9bb0d0d
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 12:01:36 +0900

    CallbackActivityを実装

--- DropTweet/src/com/example/droptweet/ui/auth/CallbackActivity.java
+++ DropTweet/src/com/example/droptweet/ui/auth/CallbackActivity.java
@@ -1,10 +1,32 @@
 package com.example.droptweet.ui.auth;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+
+import twitter4j.TwitterException;
+import twitter4j.auth.AccessToken;
 
 public class CallbackActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AccessToken token = null;
+
+        Uri uri = getIntent().getData();
+        if(uri!=null &&
+                uri.toString().startsWith("Callback://CallBackActivity")){
+            String verifier = uri.getQueryParameter("oauth_verifier");
+            try
+            {
+                token = AuthActivity._oauth.getOAuthAccessToken(AuthActivity._req, verifier);
+            }
+            catch (TwitterException e)
+            {
+                e.printStackTrace();
+                //TODO err msg
+                finish();
+            }
+        }
     }
 }
