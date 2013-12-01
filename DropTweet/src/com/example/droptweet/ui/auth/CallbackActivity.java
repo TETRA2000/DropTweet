42cb81cebc7647123043b97e1bb134b6a3aa5569
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 11:10:43 +0900

    authパッケージを作成

---
+++ DropTweet/src/com/example/droptweet/ui/auth/CallbackActivity.java
@@ -0,0 +1,10 @@
+package com.example.droptweet.ui.auth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CallbackActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
