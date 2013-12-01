6c771eeec50b0d144ef06690640b959feafb9452
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 12:24:06 +0900

    twitter.comでアプリケーションを削除し、コンシューマーキーも削除

--- DropTweet/src/com/example/droptweet/Const.java
+++ DropTweet/src/com/example/droptweet/Const.java
@@ -2,8 +2,8 @@ package com.example.droptweet;
 
 public class Const
 {
-    public static final String CONSUMER_KEY = "FCYDumxCfVRjEEPiSYe4OQ";
-    public static final String CONSUMER_SECRET = "m9qtPkU9tiQzEOzMmv929kfsxWpUmddmG0uny5rPQ";
+    public static final String CONSUMER_KEY = "";
+    public static final String CONSUMER_SECRET = "";
 
     public static final String PREF_NAME = "app";
     public static final String KEY_EULA_STATE = "read_eula";
