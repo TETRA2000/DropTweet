e35586abe150c92f25103cd2680796e4b7f5541c
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 12:42:30 +0900

    Const#KEY_TOKEN, KEY_TOKEN_SECRETを追加

--- DropTweet/src/com/example/droptweet/Const.java
+++ DropTweet/src/com/example/droptweet/Const.java
@@ -9,4 +9,6 @@ public class Const
     public static final String KEY_EULA_STATE = "read_eula";
     public static final String KEY_LAUNCHED = "launched";
 	public static final String KEY_USER_NAME = "user_name";
+    public static final String KEY_TOKEN = "token";
+    public static final String KEY_TOKEN_SECRET = "token_secret";
 }
