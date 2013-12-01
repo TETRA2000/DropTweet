6b0a366b5a5f54c2a7aec3c6c435708f49a3094a
TETRA2000 <sagittariusm25@gmail.com>
2013-12-01 12:12:16 +0900

    AccountManagerを空実装

---
+++ DropTweet/src/com/example/droptweet/ui/twitter/Account.java
@@ -0,0 +1,9 @@
+package com.example.droptweet.ui.twitter;
+
+public class Account {
+    public String key, secret;
+    public Account(String key, String secret) {
+        this.key = key;
+        this.secret = secret;
+    }
+}
---
+++ DropTweet/src/com/example/droptweet/ui/twitter/AccountManager.java
@@ -0,0 +1,15 @@
+package com.example.droptweet.ui.twitter;
+
+public class AccountManager {
+    public boolean hasAccount() {
+
+    }
+
+    public void setAccount(Account account) {
+        
+    }
+
+    public Account getAccount() {
+
+    }
+}
