621c53342d4e4a2d02e780e4f7fd1c2e42db7708
TETRA2000 <sagittariusm25@gmail.com>
2013-12-03 08:46:33 +0900

    AccountTestを作成

---
+++ DropTweetTest/src/com/example/droptweet/test/twitter/AccountTest.java
@@ -0,0 +1,24 @@
+package com.example.droptweet.test.twitter;
+
+import com.example.droptweet.ui.twitter.Account;
+
+import android.test.AndroidTestCase;
+
+public class AccountTest extends AndroidTestCase {
+	public void testEqualsがTrueの場合() {
+		Account a1 = new Account("Jhon", "abc", "def");
+		Account a2 = new Account("Jhon", "abc", "def");
+		
+		assertEquals(a1, a2);
+	}
+	
+	public void testEqualsがFalseの場合() {
+		Account a1 = new Account("Jhon", "abc", "def");
+		Account a2 = new Account("Jack", "123", "456");
+		
+		//TODO 他のパターンも追加
+		
+		assertFalse(a1.equals(a2));
+		assertFalse(a2.equals(a1));
+	}
+}
