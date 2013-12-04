220dc54c84685694a90c38629ac81a5808b9bf1f
TETRA2000 <sagittariusm25@gmail.com>
2013-12-04 23:38:34 +0900

    MainActivity#onCreateを作成

--- DropTweet/src/com/example/droptweet/ui/MainActivity.java
+++ DropTweet/src/com/example/droptweet/ui/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.droptweet.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.droptweet.R;
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
 }
