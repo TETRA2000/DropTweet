ce7debbcebb6ca3cd17eba833654da809384a742
TETRA2000 <sagittariusm25@gmail.com>
2013-12-04 19:55:06 +0900

    CallbackActivityにてtoken保存周りを実装 (UIスレッドでのネットワーク処理でエラー発生中)

--- DropTweet/src/com/example/droptweet/ui/auth/CallbackActivity.java
+++ DropTweet/src/com/example/droptweet/ui/auth/CallbackActivity.java
@@ -6,25 +6,55 @@ import android.os.Bundle;
 
 import twitter4j.TwitterException;
 import twitter4j.auth.AccessToken;
+import com.example.droptweet.ui.twitter.*;
+import twitter4j.*;
+import com.example.droptweet.*;
+import android.content.*;
+import com.example.droptweet.ui.*;
+import android.widget.*;
 
 public class CallbackActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		
+		Toast.makeText(this, "callback", Toast.LENGTH_LONG).show();
 
         AccessToken token = null;
 
         Uri uri = getIntent().getData();
+		
+		Toast.makeText(this, uri.toString(), Toast.LENGTH_LONG).show();
+		
         if(uri!=null &&
                 uri.toString().startsWith("callback://CallbackActivity")){
             String verifier = uri.getQueryParameter("oauth_verifier");
             try
             {
                 token = AuthActivity._oauth.getOAuthAccessToken(AuthActivity._req, verifier);
+				
+				//set token
+				Twitter twitter = TwitterFactory.getSingleton();
+				twitter.setOAuthConsumer(Const.CONSUMER_KEY, Const.CONSUMER_SECRET);
+				twitter.setOAuthAccessToken(token);
+				
+				//save token
+				Account account = new Account(token.getScreenName(), token.getToken(), token.getTokenSecret());
+				AccountManager manager = new AccountManager(this);
+				manager.setAccount(account);
+				
+				Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
+				
+				//go to MainActivity
+				Intent intent = new Intent(this, MainActivity.class);
+				startActivity(intent);
+				finish();
             }
             catch (TwitterException e)
             {
                 e.printStackTrace();
                 //TODO err msg
+				Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show();
                 finish();
             }
         }
