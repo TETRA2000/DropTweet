7e5e175cf021b185fa8be3861ce77a38bda8f4a7
TETRA2000 <sagittariusm25@gmail.com>
2013-12-04 19:52:44 +0900

    AccountManagerTestを作成(一部未完成)

---
+++ DropTweetTest/src/com/example/droptweet/test/twitter/AccountManagerTest.java
@@ -0,0 +1,53 @@
+package com.example.droptweet.test.twitter;
+
+import com.example.droptweet.Const;
+import com.example.droptweet.ui.twitter.Account;
+import com.example.droptweet.ui.twitter.AccountManager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.test.InstrumentationTestCase;
+
+public class AccountManagerTest extends InstrumentationTestCase {
+	private SharedPreferences mPref;
+	private AccountManager sut;
+	
+	@Override
+	public void setUp() {
+		Context context = getInstrumentation().getContext();
+		mPref = context.getSharedPreferences(Const.PREF_NAME, Context.MODE_PRIVATE);		
+		resetPref();
+		
+		sut = new AccountManager(context);
+	}
+	
+	@Override
+	public void tearDown() {
+		resetPref();
+	}
+	
+	private void resetPref() {
+		SharedPreferences.Editor editor = mPref.edit();
+		editor.remove(Const.KEY_USER_NAME);
+		editor.remove(Const.KEY_TOKEN);
+		editor.remove(Const.KEY_TOKEN_SECRET);
+		editor.commit();
+	}
+	
+	public void test正しくアカウントが保存出来る() {
+		Account expected = new Account("Android", "key", "secret");
+		
+		sut.setAccount(expected);
+		Account actual = sut.getAccount();
+		
+		assertEquals(expected, actual);
+	}
+	
+	public void hasAccountが正しい値を返す() {
+		fail();
+	}
+	
+	public void removeAccountが正しい値を返す() {
+		fail();
+	}
+}
