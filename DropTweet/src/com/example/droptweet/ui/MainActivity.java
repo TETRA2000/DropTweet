93bf5f32eb7eaac385dce0df6966f31c32965384
TETRA2000 <sagittariusm25@gmail.com>
2013-12-08 01:05:05 +0900

    MainActivity起動時にサービスを起動

--- DropTweet/src/com/example/droptweet/ui/MainActivity.java
+++ DropTweet/src/com/example/droptweet/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.droptweet.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.Window;
 
 import com.example.droptweet.R;
+import com.example.droptweet.SensorService;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +17,10 @@ public class MainActivity extends Activity {
 
         Window window = getWindow();
         window.setFormat(PixelFormat.RGBA_8888);
+
+        //サービスを起動
+        Intent intent = new Intent(this, SensorService.class);
+        startService(intent);
 
         setContentView(R.layout.activity_main);
     }
