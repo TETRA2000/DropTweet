90cfea5844ada42975f9dfef8db7f9b7d20e685c
TETRA2000 <sagittariusm25@gmail.com>
2013-12-11 18:09:05 +0900

    AsyncTask(恐らく意味がない)

--- DropTweet/src/com/example/droptweet/ui/auth/AuthActivity.java
+++ DropTweet/src/com/example/droptweet/ui/auth/AuthActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
 import com.example.droptweet.Const;
 import com.example.droptweet.R;
+
+import java.util.Objects;
 
 import twitter4j.TwitterException;
 import twitter4j.auth.OAuthAuthorization;
@@ -55,5 +58,36 @@ public class AuthActivity extends Activity implements Runnable {
         String _uri;
         _uri = _req.getAuthorizationURL();
         startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(_uri)), 0);
+    }
+
+    class AuthTask extends AsyncTask<Objects, Objects, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Objects... params) {
+            Configuration conf = ConfigurationContext.getInstance();
+
+            _oauth = new OAuthAuthorization(conf);
+            _oauth.setOAuthConsumer(Const.CONSUMER_KEY, Const.CONSUMER_SECRET);
+            try {
+                _req = _oauth.getOAuthRequestToken("callback://CallbackActivity");
+                return true;
+            } catch (TwitterException e) {
+                e.printStackTrace();
+                // TODO エラーメッセージ
+                finish();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if(success) {
+                String _uri;
+                _uri = _req.getAuthorizationURL();
+                startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(_uri)), 0);
+            } else {
+                finish();
+            }
+        }
     }
 }
