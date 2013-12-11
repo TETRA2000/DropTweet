276b5db742ccdb6b99a16fccb4651b687ab8c222
TETRA2000 <sagittariusm25@gmail.com>
2013-12-11 18:09:54 +0900

    ハッシュタグを定数に

--- DropTweet/src/com/example/droptweet/Const.java
+++ DropTweet/src/com/example/droptweet/Const.java
@@ -11,4 +11,5 @@ public class Const
 	public static final String KEY_USER_NAME = "user_name";
     public static final String KEY_TOKEN = "token";
     public static final String KEY_TOKEN_SECRET = "token_secret";
+    public static final String HASH_TAG = "#drop_tweet";
 }
