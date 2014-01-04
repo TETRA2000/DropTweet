3e1899566b37d471bb119561f1b3cfec995c34a8
TETRA2000 <sagittariusm25@gmail.com>
2014-01-04 13:00:08 +0900

    Twitterに再度コンシューマキーを設定する問題を修正

--- DropTweet/src/jp/tetra2000/droptweet/twitter/TwitterManager.java
+++ DropTweet/src/jp/tetra2000/droptweet/twitter/TwitterManager.java
@@ -1,7 +1,6 @@
 package jp.tetra2000.droptweet.twitter;
 
 import jp.tetra2000.droptweet.Const;
-
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
@@ -10,8 +9,10 @@ public class TwitterManager {
     private static Twitter mTwitter;
 
     public static void init(Account account) {
-        mTwitter = TwitterFactory.getSingleton();
-        mTwitter.setOAuthConsumer(Const.CONSUMER_KEY, Const.CONSUMER_SECRET);
+        if(mTwitter==null) {
+            mTwitter = TwitterFactory.getSingleton();
+            mTwitter.setOAuthConsumer(Const.CONSUMER_KEY, Const.CONSUMER_SECRET);
+        }
         AccessToken token = new AccessToken(account.token, account.secret);
         mTwitter.setOAuthAccessToken(token);
     }
