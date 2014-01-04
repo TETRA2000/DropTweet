d495e3960c773c8276c087466fce3f5b791cddf8
TETRA2000 <sagittariuam25@gmail.com>
2014-01-04 17:45:37 +0900

    ログアウトした際にサービスを停止

--- DropTweet/src/jp/tetra2000/droptweet/ui/SettingsActivity.java
+++ DropTweet/src/jp/tetra2000/droptweet/ui/SettingsActivity.java
@@ -62,6 +62,9 @@ public class SettingsActivity extends PreferenceActivity {
                 // ログアウト
                 AccountManager manager = new AccountManager(SettingsActivity.this);
                 manager.removeAccount();
+				
+				Intent intent = new Intent(SettingsActivity.this, SensorService.class);
+				stopService(intent);
 
                 finish();
             }
